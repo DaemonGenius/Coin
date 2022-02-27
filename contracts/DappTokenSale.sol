ea375d52a1c246654c3bb094b25ce189a08e2de1
Christian <christian.jhardy.steinmann@gmail.com>
2022-02-27 08:34:49 +0200

    added token sale contract

---
+++ contracts/DappTokenSale.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./DappToken.sol";
+
+contract DappTokenSale {
+
+    address admin;
+    DappToken public tokenContract;
+    uint public tokenPrice;
+
+     constructor(DappToken _tokenContract, uint _tokenPrice) {
+         admin = msg.sender;
+         tokenContract = _tokenContract;
+         tokenPrice = _tokenPrice;
+
+        
+    }
+}
